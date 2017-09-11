943a94906867d980977e386dadfbb657c2f701fe
Pavan Divekar <pavandivekar@Pavans-MacBook-Air.local>
2017-09-11 23:47:55 +0530

    navigation bar is created

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,3 +1,10 @@
+console.log("script js is loaded");
+function login(){
+    console.log("inside the login function");
+    document.getElementById("login").textContent = "Pavan";
+    document.getElementById("signup").style.display = "none";
+}
 (function(){
-    console.log("onload executable fucntion");
+    console.log("inside the IFI");
+
 });
